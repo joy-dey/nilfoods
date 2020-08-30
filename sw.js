1caa866f4f4b6a987ed423526453a73f40d633e0
Drubajyoti Debnath <60551363+unknown009d@users.noreply.github.com>
2020-08-30 15:10:24 +0530

    Converted to PWA

---
+++ sw.js
@@ -0,0 +1,8 @@
+/* Install Service Worker */
+self.addEventListener("install", (evt) => {});
+
+/* Activate the Service Worker */
+self.addEventListener("activate", (evt) => {});
+
+/* Fetch Event */
+self.addEventListener("fetch", (evt) => {});
